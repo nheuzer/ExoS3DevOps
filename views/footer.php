dc7e6bfe3ec695b31ceb82093cb9bf81eaedbfbb
unknown <nina.heuzer@student.vinci.be>
2020-09-29 18:09:33 +0200

    add line conflit A

--- views/footer.php
+++ views/footer.php
@@ -1,4 +1,5 @@
 		<div id="pied">
+            Conflit A
 			<strong>Excellente journée qu'aujourd'hui le <?php echo $date ?></strong> :: 
 			<?php $time_end = microtime(true);
 				  $time = number_format(($time_end - $time_start)*1000,6);
